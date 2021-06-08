227acc1f10742d493706e7fb626707dcaefc164e
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-06-08 16:46:54 +0100

    DB with SQLAlchemy

--- server.py
+++ server.py
@@ -1,14 +1,31 @@
+"""
+Main module of the server file
+"""
+
+# 3rd party moudles
 from flask import render_template
-import connexion
 
-app = connexion.App(__name__, specification_dir="./")
+# local modules
+import config
 
-# read the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
 
-@app.route("/")
+# Get the application instance
+connex_app = config.connex_app
+
+# Read the swagger.yml file to configure the endpoints
+connex_app.add_api("swagger.yml")
+
+
+# create a URL route in our application for "/"
+@connex_app.route("/")
 def home():
-  return render_template("home.html")
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    connex_app.run(debug=True)
